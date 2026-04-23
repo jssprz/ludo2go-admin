614b589e5c25554ab8a463a13285a35b23f3ab2b
Jesus Perez-Martin <jperezmartin90@gmail.com>
2026-04-23 10:34:29 -0400

    minor

--- app/(dashboard)/nav-item.tsx
+++ app/(dashboard)/nav-item.tsx
@@ -26,7 +26,7 @@ export function NavItem({
         <Link
           href={href}
           className={clsx(
-            'flex h-6 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-5 md:w-8',
+            'flex h-7 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-6 md:w-8',
             {
               'bg-accent text-black': pathname === href
             }
